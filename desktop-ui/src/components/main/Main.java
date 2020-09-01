ddf809f4d2e52ea93a6dc11d5066dcb1e1891b3e
oriAdler <57869309+oriAdler@users.noreply.github.com>
2020-09-01 22:48:56 +0300

    Ex2 - Desktop Application: First steps.. - Main Menu structure. - Jaxb Scheme is updated.

---
+++ desktop-ui/src/components/main/Main.java
@@ -0,0 +1,41 @@
+package components.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import engine.Engine;
+import engine.EngineImpl;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+
+        // Load main fxml
+        URL mainFXML = getClass().getResource("sdm.fxml");
+        loader.setLocation(mainFXML);
+        BorderPane root = loader.load();
+
+        // Wire up controller
+        MainController mainController = loader.getController();
+        Engine engine = new EngineImpl(mainController);
+        mainController.setPrimaryStage(primaryStage);
+        mainController.setEngine(engine);
+
+        // Set stage:
+        primaryStage.setTitle("Super Duper Market");
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+}
